c5a8d8d3eea964d080cfe3e25a8229a22399a9a7
takeuchi-shogo <takeuchi-shogo@users.noreply.github.com>
2026-03-09 17:32:59 +0900

    ✨ feat(nvim): enable Octo.nvim with custom keymaps
    - Remove disabled keymaps from disable.lua
    - Add <leader>O prefix keymaps for Issue/PR/Review
    - Lazy-load on Octo command

---
+++ .config/nvim/lua/plugins/editor/octo.lua
@@ -0,0 +1,13 @@
+return {
+  "pwntester/octo.nvim",
+  cmd = "Octo",
+  keys = {
+    { "<leader>Oi", "<cmd>Octo issue list<CR>", desc = "Issue list (Octo)" },
+    { "<leader>Ois", "<cmd>Octo issue search<CR>", desc = "Issue search (Octo)" },
+    { "<leader>Op", "<cmd>Octo pr list<CR>", desc = "PR list (Octo)" },
+    { "<leader>Ops", "<cmd>Octo pr search<CR>", desc = "PR search (Octo)" },
+    { "<leader>Or", "<cmd>Octo review start<CR>", desc = "Start review (Octo)" },
+    { "<leader>Os", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+  },
+  opts = {},
+}
