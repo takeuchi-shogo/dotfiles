686e9458b505896116dcb1b1050deb9944caf115
takeuchi-shogo <takeuchi-shogo@users.noreply.github.com>
2026-03-09 18:02:34 +0900

    🐛 fix(nvim): disable LazyVim Octo Extra default keymaps
    - Disable <leader>g* keymaps from LazyVim Octo Extra
    - Keep Extra import for treesitter/picker/autocmd benefits
    - Custom <leader>O keymaps remain as sole entry points

--- .config/nvim/lua/plugins/editor/octo.lua
+++ .config/nvim/lua/plugins/editor/octo.lua
@@ -2,12 +2,19 @@ return {
   "pwntester/octo.nvim",
   cmd = "Octo",
   keys = {
+    -- カスタムキーマップ (<leader>O プレフィックス)
     { "<leader>Oi", "<cmd>Octo issue list<CR>", desc = "Issue list (Octo)" },
     { "<leader>Ois", "<cmd>Octo issue search<CR>", desc = "Issue search (Octo)" },
     { "<leader>Op", "<cmd>Octo pr list<CR>", desc = "PR list (Octo)" },
     { "<leader>Ops", "<cmd>Octo pr search<CR>", desc = "PR search (Octo)" },
     { "<leader>Or", "<cmd>Octo review start<CR>", desc = "Start review (Octo)" },
     { "<leader>Os", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+    -- LazyVim Octo Extra のデフォルトキーマップを無効化
+    { "<leader>gi", false },
+    { "<leader>gI", false },
+    { "<leader>gp", false },
+    { "<leader>gP", false },
+    { "<leader>gr", false },
+    { "<leader>gS", false },
   },
-  opts = {},
 }
